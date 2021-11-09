9ddbef09d6901bdf758bf007f190d8e83644d052
Cole Roper <coleroper26@gmail.com>
2021-11-09 03:51:43 -0600

    Fix setup-udhcpd.sh

--- util/setup-udhcpd.sh
+++ util/setup-udhcpd.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # CONFIG
 
@@ -12,13 +12,15 @@ cp /pixy/config/udhcpd.conf /etc/udhcpd.conf
 # DNS
 
 ## get dns servers provided by eth0
-DNS=$(grep -h "nameserver" "/home/pi/tmp.txt" | sed "s/^.* //")
+# DNS=$(grep -h "nameserver" "/etc/resolv.conf" | sed "s/^.* //")
+DNS=$(grep -h "nameserver" "/etc/resolv.conf" | sed "s/^.* //" | tr "\n" " ")
+# DNS="${DNS//\\n/ }"
 
 ## if no dns servers were provided, use 8.8.8.8 and 8.8.4.4
 ([[ ! -z "$DNS" ]]) || DNS="8.8.8.8 8.8.4.4"
 
 ## append the dns servers to /etc/udhcpd.conf
-echo "option dns ${DNS}" >> "/etc/udhcpd.conf"
+echo $"option dns ${DNS}" >> "/etc/udhcpd.conf"
 
 
 # SERVICE
