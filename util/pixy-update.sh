a1531d040f1bbe889e95461b5acf885ce5fc797e
Cole Roper <coleroper26@gmail.com>
2021-11-09 14:06:34 -0600

    Fix bug where base config files were deleted.

--- util/pixy-update.sh
+++ util/pixy-update.sh
@@ -14,7 +14,7 @@ echo -e "[PIXY-UPDATE]\tDONE\tservices stopped\n"
 
 ## network interfaces
 mv /etc/network/interfaces /etc/network/interfaces.bak  # backup
-mv /pixy/config/interfaces /etc/network/interfaces      # replace
+cp /pixy/config/interfaces /etc/network/interfaces      # replace
 systemctl start networking.service                      # start
 /pixy/util/iptables-setup.sh                            # setup iptables
 echo -e "[PIXY-UPDATE]\tDONE\tnetwork interfaces\n"
@@ -25,6 +25,6 @@ echo -e "[PIXY-UPDATE]\tDONE\tudhcpd\n"
 
 ## squid
 mv /etc/squid/squid.conf   /etc/squid/squid.conf.bak    # backup
-mv /pixy/config/squid.conf /etc/squid/squid.conf        # replace
+cp /pixy/config/squid.conf /etc/squid/squid.conf        # replace
 systemctl start squid.service                           # start
 echo -e "[PIXY-UPDATE]\tDONE\tservices started\n"
