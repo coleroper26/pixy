a3e5b5731f5056e193410ff4ce5a1cef86f027f2
cole roper <coleroper26@gmail.com>
2021-12-18 16:11:12 -0600

    Fin

--- util/iptables-setup.sh
+++ util/iptables-setup.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+##/pixy/util/iptables-setup.sh
 
 # setup
 echo 1 > /proc/sys/net/ipv4/ip_forward  # enable ipv4 packet forwarding
@@ -10,9 +11,9 @@ iptables -F         # flush primary table
 iptables -t nat -F  # flush nat table
 iptables -X         # delete user-defined chains
 
-## transparent out of eth0 from eth1
+## eth0 -> eth1 transparent
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-## redirect all port 80 traffic to port 3128
+## from eth1 IP:80 -> IP:3128
 iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 3128
 
